05b284661fd0f8afd43e965e7529a3699b0bf26d
martivo <mardicas@gmail.com>
2026-02-27 11:04:44 +0200

    Remove comment

--- pkg/controllers/webhook/controller.go
+++ pkg/controllers/webhook/controller.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sort"
 	"sync"
 	"time"
 
@@ -627,7 +628,6 @@ func (c *controller) reconcileValidatingWebhookConfiguration(ctx context.Context
 	if err != nil {
 		return err
 	}
-	# # Sort webhooks by name for deterministic ordering (fixes #15318)
 	sort.Slice(desired.Webhooks, func(i, j int) bool { return desired.Webhooks[i].Name < desired.Webhooks[j].Name })
 	observed, err := c.vwcLister.Get(desired.Name)
 	if err != nil {
@@ -659,7 +659,6 @@ func (c *controller) reconcileMutatingWebhookConfiguration(ctx context.Context, 
 	if err != nil {
 		return err
 	}
-	# # Sort webhooks by name for deterministic ordering (fixes #15318)
 	sort.Slice(desired.Webhooks, func(i, j int) bool { return desired.Webhooks[i].Name < desired.Webhooks[j].Name })
 	observed, err := c.mwcLister.Get(desired.Name)
 	if err != nil {
